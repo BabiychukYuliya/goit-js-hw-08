43ebf8ca8f8e26e7e7ea3d3ce44fb3115c394e1d
BabiychukYuliya <110289455+BabiychukYuliya@users.noreply.github.com>
2022-12-22 14:00:32 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
